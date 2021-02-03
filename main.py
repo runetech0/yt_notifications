caab141e10d870935f2e984fff9a49b9a448abc1
Rehman Ali <rehmanali.9442289@gmail.com>
2021-02-03 21:52:58 +0500

    Youtube notifications for discord

---
+++ main.py
@@ -0,0 +1,50 @@
+from bottle import route, run, request, default_app, abort
+import xml.etree.ElementTree as XML
+
+secret = 'some_url_safe_secret'
+
+namespaces = {
+    'yt': 'http://www.youtube.com/xml/schemas/2015',
+    'xmlns': 'http://www.w3.org/2005/Atom'
+}
+
+
+@route('/callback', method='get')
+@route('/callback', method='post')
+def index():
+    print(f'Got a {request.method} request!')
+    if request.method == 'POST':
+        response = request.body.read().decode('utf-8')
+        root = XML.fromstring(response)
+        print(root)
+
+        for entry in root.findall('xmlns:entry', namespaces=namespaces):
+            video_id = entry.find('yt:videoId', namespaces=namespaces).text
+            title = entry.find('xmlns:title', namespaces=namespaces).text
+            link = entry.find('xmlns:link', namespaces=namespaces).get('href')
+            print(video_id, title, link)
+
+        # Do Something Here
+
+        return 'Goodly'
+
+    # try:
+    #     mode = request.query['hub.mode']
+    #     challenge = request.query['hub.challenge']
+    #     verify_token = request.query['hub.verify_token']
+    # except KeyError:
+    #     abort(404)
+
+    # if mode == 'subscribe' and verify_token == secret:
+    #     return challenge
+    # abort(404)
+    if request.method == 'GET':
+        print('Got GET request')
+
+
+if __name__ == '__main__':
+    """For Development"""
+    run(host='0.0.0.0', port=8889, debug=True)
+
+# For Production
+app = default_app()
